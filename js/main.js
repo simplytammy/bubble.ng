7586d48db4725c4e8f474dde18d4b6ac7405523b
simplytammy <ogurinkabenjamin@gmail.com>
2019-07-05 14:34:00 +0100

    New Commit

--- js/main.js
+++ js/main.js
@@ -1,22 +1,9 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();
 
-// // Hire developers
-// const hero = document.querySelector('.hero-form');
-// hero.addEventListener('click',activeForm);
-// const active = document.querySelector('#form-event');
-// function activeForm(){
-//   active.focus();
-// }
-
-// document.getElementById('email').addEventListener('blur',validateEmail);
-
-//  function validateEmail(){
-//   const email = document.getElementById('email');
-//   const regEx = /^([a-zA-Z0-9_\.\-]+)@([a-zA-Z0-9_\.\-]+)\.([a-zA-Z]{2,})$/
-//   if(!regEx.test(email.value)){
-//     email.classList.add('is-invalid');
-//     console.log('invalid')
-//   }else{
-//     email.classList.remove('is-invalid');
-//   }
-// }
-
+    if (scroll >= 500) {
+        $(".navbar").addClass("nav-bg");
+    } else {
+        $(".navbar").removeClass("nav-bg");
+    }
+});
